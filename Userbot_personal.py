65badf4541da405435cd22379d53122524339af5
AleDev01 <84560010+aleDev01@users.noreply.github.com>
2022-03-07 16:42:48 +0100

    Update Userbot_personal.py
    fix code + testing

--- Userbot_personal.py
+++ Userbot_personal.py
@@ -1,16 +1,17 @@
 from telethon import TelegramClient, events, sync
 from telethon import Button
 from telethon import functions, types
+from datetime import datetime
 
 import os
 import asyncio
+import telethon
 
 
-API_ID = 1289
-API_HASH ="aa1ot8"
+API_ID = 77362
+API_HASH ="aa1iy8"
 
 userbot_personal = TelegramClient("userbot_personal", API_ID, API_HASH)
-
 
 
 
@@ -20,7 +21,7 @@ async def usbot():
   
   @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.status"))
   async def statususerbot(event):
-    await event.edit("userbot online")
+    await event.edit("userbot personal online")
 
   @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.name"))
   async def nameprofilyou(event):
@@ -37,7 +38,55 @@ async def usbot():
     await asyncio.sleep(3)
 
 
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.reboot"))
+  async def reboot(event):
+    await event.delete()
+    import os, sys, threading
+    os.system("clear")
+  
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    thereading.Thread(target=_restart, args=(bot, msg)).start()
 
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r".ping"))
+  async def _ping(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**Pong!**")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("**Pong!**\n`{}`".format(ms))
+
+  @userbot_personal.on(events.NewMessage(outgoing=True))
+  async def t_p(event):
+    if event.text == ".d":
+      await event.edit("d lol")
+
+
+
+  #@userbot_personal.on(events.Raw)
+  #async def handler(update):
+    # Print all incoming updates
+    #await userbot_personal.send_message("me",update.stringify())
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.delm"))
+  async def _delm(event):
+    await event.delete()
+    d = await event.get_reply_message()
+    await d.delete()
+    await event.respond("deleted message")
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.del"))
+  async def _delm(event):
+    await event.delete()
+    d = await event.get_reply_message()
+    await d.delete()
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.version_telethon"))
+  async def versiontelethon(e):
+    g = (telethon.__version__)
+    await e.edit(f"version telethon -> {g}")
 
 
   
